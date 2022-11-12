c0b1a47bc4438100fc90a4da01b251c62307ba4c
Azka Noreen <azka.noreen6@gmail.com>
2022-11-12 17:51:11 +0500

    stop log also implemented

--- app/src/main/java/com/example/studentapp/lab14/ServiceAct.java
+++ app/src/main/java/com/example/studentapp/lab14/ServiceAct.java
@@ -4,17 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.studentapp.R;
 
 public class ServiceAct extends AppCompatActivity {
 
+    Button start,stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
 
-        Intent intent=new Intent(this,MyService.class);
-        startService(intent);
+        start=findViewById(R.id.start);
+        stop=findViewById(R.id.stop);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ServiceAct.this,MyService.class);
+                startService(intent);
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ServiceAct.this,MyService.class);
+                stopService(intent);
+            }
+        });
+
+
+
+
+
     }
 }
